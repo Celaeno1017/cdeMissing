78a73ddb0c3452b8fff219828004df9915d6e13b
Jiangshan Zhang <43353764+Celaeno1017@users.noreply.github.com>
2025-12-20 20:08:43 -0800

    Update cde_fit.R

--- R/cde_fit.R
+++ R/cde_fit.R
@@ -127,7 +127,7 @@ cde_fit <- function(
 
     out <- list(
       fit = fit,
-      engine = list("lme4",form_fix, merge_random_list_by_group(form_random, stats::as.formula(paste("~ mis_any-1 | id"))))
+      engine = list("lme4",form_fix, merge_random_list_by_group(form_random, stats::as.formula(paste("~ mis_any-1 | id")))),
       family = family,
       data_aug = if (isTRUE(return_augmented_data)) dat_aug else NULL,
       call = match.call()
